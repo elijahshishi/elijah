a63a4c788cd665262238118e87281a336511e8a3
elijahshishi <elijahjadevillacastin@gmail.com>
2025-09-20 00:35:52 +0800

    Add files via upload

---
+++ Final Project - VILLACASTIN/script.js
@@ -0,0 +1,8 @@
+function showWelcome() {
+    alert("Welcome to my Portfolio Website!");
+  }
+  
+  function sendMessage() {
+    const name = document.getElementById("name").value;
+    alert("Thank you, " + name + "! Your message has been sent.");
+  }
